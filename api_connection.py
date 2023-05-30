4694d4bdc640cf1f28e5e343256f2b6e797125ec
h4sski <h4sski.programming.gmail.com>
2023-05-30 12:27:59 +0200

    api connection works, defauls Krakow

--- api_connection.py
+++ api_connection.py
@@ -1,3 +1,17 @@
 # https://home.openweathermap.org/api_keys
+import requests
+
 
 API_KEY = 'a737dead0c6e306a297117f907aa102d'
+
+
+def get_weather_json(lat, lon):
+    response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&mode=jason&units=metric')
+    # city json based on https://openweathermap.org/api/geocoding-api
+    return response.json()
+
+
+def get_city_json(city_name='Krakow', country_code='PL', limit=5):
+    response = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{country_code}&limit={limit}&appid={API_KEY}')
+    # weather json based on https://openweathermap.org/current
+    return response.json()[0]
--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+import requests
 from flask import Flask, render_template
+
+from api_connection import get_weather_json, get_city_json
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    
+    # city json based on https://openweathermap.org/api/geocoding-api
+    city_json = get_city_json()
+    
+    # weather json based on https://openweathermap.org/current
+    weather_json = get_weather_json(lat=city_json['lat'], lon=city_json['lon'])
+    
+    return render_template('index.html', weather_json=weather_json)
 
 
 if __name__ == '__main__':
